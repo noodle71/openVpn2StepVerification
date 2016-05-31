3006dc594dd182a65af8ae8ec1c6f697b5f7528b
Angel Fernández Camba <noodle71@gmail.com>
2016-05-31 13:16:20 +0200

    First commit

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
-#USE
-./googleAuthomationStart.sh openvpn_username openvpn_password google_auth_secret openvpn_config_file
-#INSTALL EXPECT
+#openVpn2StepVerification
+
+##Description
+
+Relieves the pain using openvpn with Google two steps verification.
+
+It generates the Google Auth Token automatically.
+
+###USE
+
+```bash
+./googleAuthomationStart.sh   openvpn_username   openvpn_password   google_auth_secret   openvpn_config_file
+```
+
+*IMPORTANT* If you need to enter the sudo password when connect to openvpn then:
+
+```bash
+./googleAuthomationStart.sh   openvpn_username   openvpn_password   google_auth_secret   openvpn_config_file   sudo_password
+```
+
+###INSTALL EXPECT
+
+```bash
 sudo apt-get install expect
-#INSTALL PIP
+```
+
+###INSTALL PIP
+
+```bash
 sudo apt-get install python-pip python-dev build-essential
 sudo -H pip install --upgrade pip
 sudo pip install --upgrade virtualenv
-#INSTALL ONETIMEPASS
+```
+
+###INSTALL ONETIMEPASS
+
+```bash
 sudo pip install onetimepass
+```
--- googleAuthomation.sh
+++ googleAuthomation.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/expect -f
 
 # Variables
+set timeout -1
+set nArgs [llength $argv]
 set user [lindex $argv 0]
 set pass [lindex $argv 1]
 set googleAuthSecret [lindex $argv 2]
 set openVpnConfigFile [lindex $argv 3]
+
 set googleAuthToken [exec python createToken.py $googleAuthSecret]
+if { $nArgs >= 4 } {
+  # Access to device
+  spawn sudo openvpn $openVpnConfigFile
 
-# Access to device
-spawn sudo openvpn $openVpnConfigFile
+  if { $nArgs > 4 } {
+    set sudoPass [lindex $argv 4]
+    expect "\[sudo\]*"
+    send -- "$sudoPass\r"
+  }
 
-expect "*Enter Auth Username:*"
-send -- "$user\r"
+  expect "\[Username\]*"
+  send -- "$user\r"
 
-expect "*Enter Auth Password:*"
-send -- "$pass\r"
+  expect "\[Password\]*"
+  send -- "$pass\r"
 
-expect "*Response:*"
-send -- "$googleAuthToken\r"
+  expect "\[Response\]*"
+  send -- "$googleAuthToken\r"
 
-interact
-close
+  interact
+  close
+} else {
+  puts "Usage: ./googleAuthomationStart.sh   openvpn_username   openvpn_password   google_auth_secret   openvpn_config_file   sudo_password";
+}
